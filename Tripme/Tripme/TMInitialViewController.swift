6185db18c9e1fea3c50539b0e8dbeb2a05d341a0
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-10-30 15:24:35 -0400

    Criando a classe da tela inicial.

---
+++ Tripme/Tripme/TMInitialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TMInitialViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 10/30/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMInitialViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    
+}
