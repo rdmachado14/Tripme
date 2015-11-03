1a648f1a2538a1e49933b4a9c0153d2f1ca7768b
Carlos Amadheus Souza Araújo <amadheus95@gmail.com>
2015-11-03 09:54:14 -0400

    menu inicial..

---
+++ Tripme/Tripme/TMMenuItensCell.swift
@@ -0,0 +1,38 @@
+//
+//  TMMenuItensCell.swift
+//  Tripme
+//
+//  Created by Carlos Amadheus Souza Araujo on 03/11/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMMenuItensCell: UICollectionViewCell
+{
+    // MARK: - Public API
+    var trips: Trips!{
+        didSet {
+            updateUI()
+        }
+    }
+    
+    
+    // MARK: - Private
+    
+    @IBOutlet weak var featuredImageView: UIImageView!
+    @IBOutlet weak var interestTitleLabel: UILabel!
+    
+    private func updateUI()
+    {
+        interestTitleLabel?.text! = trips.title
+        featuredImageView?.image! = trips.featuredImage
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+    }
+}
---
+++ Tripme/Tripme/TMMenuViewController.swift
@@ -0,0 +1,79 @@
+//
+//  TMMenuViewController.swift
+//  Tripme
+//
+//  Created by Carlos Amadheus Souza Araujo on 03/11/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMMenuViewController: UIViewController {
+    @IBOutlet weak var imageView : UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var currentUserProfileImageButton: UIButton!
+    @IBOutlet weak var currentUserFullNameButton: UIButton!
+    
+    // MARK: - UICollectionViewDataSource
+    private var trips = Trips.createInterests()
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private struct Storyboard {
+        static let CellIdentifier = "Trips Cell"
+    }
+}
+
+extension TMMenuViewController : UICollectionViewDataSource
+{
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return trips.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        let cell:TMMenuItensCell = collectionView.dequeueReusableCellWithReuseIdentifier(Storyboard.CellIdentifier, forIndexPath: indexPath) as! TMMenuItensCell
+        
+        cell.trips = self.trips[indexPath.item]
+        //cell.featuredImageView.im
+        
+        return cell
+    }
+}
+
+extension TMMenuViewController : UIScrollViewDelegate
+{
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+    {
+        let layout = self.collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.memory
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+        targetContentOffset.memory = offset
+    }
+}
+
+extension TMMenuViewController: UICollectionViewDelegate
+{
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+    }
+}
