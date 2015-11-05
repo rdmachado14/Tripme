6d672b9fc6156fe7ca0ef8f6dcd1c1a92e907886
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-11-05 11:10:30 -0400

    Criando a classe do perfil.

---
+++ Tripme/Tripme/TMProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TMProfileViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 11/5/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
