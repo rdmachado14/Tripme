b4080eddfb36008f987c454ecd087dec8391600e
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-11-06 09:48:01 -0400

    Implementando o método contrutor.

--- Tripme/Tripme/TMProfileTrips.swift
+++ Tripme/Tripme/TMProfileTrips.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TMProfileTrips
+{
+    var tripName: String! // nome da viagem
+    var user: String! // nome do usuário
+    var moneyRaised: Double! // dinheiro arrecadado
+    var moneyTotal: Double! // total para ser arrecadado
+    var imageUser: UIImage! // imagem do usuário
+    
+    // método construtor
+    init(tripName: String, user: String, moneyRaised: Double, moneyTotal: Double, imageUser: UIImage)
+    {
+        self.tripName = tripName
+        self.user = user
+        self.moneyRaised = moneyRaised
+        self.moneyTotal = moneyTotal
+        self.imageUser = imageUser
+    }
+    
+    
+    
+    
+}
