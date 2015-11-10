ae15bbe1f96e29f62db4be2e167f572e9d5667da
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-11-10 15:56:48 -0400

    Adicionando a classe para fazer a busca no Parse.

---
+++ Tripme/Tripme/TMSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TMSearchViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 11/10/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMSearchViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+      
+    }
+    
+
+
+
+}
