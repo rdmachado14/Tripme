1c267c26eb108cc5479c50b8fe8c60a5a7d75fde
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-11-24 11:36:23 -0400

    Criando a classe de criar uma viagem.

---
+++ Tripme/Tripme/TMTripProjectViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TMTripProjectViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 11/24/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMTripProjectViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
