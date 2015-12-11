871dc7e2b8ebb7e7f9fd4433f0097a3ce2959512
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-12-11 15:52:48 -0400

    Criando a classe da tela de reeviar senha.

---
+++ Tripme/Tripme/TMRecoveryPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TMRecoveryPasswordViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 12/11/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMRecoveryPasswordViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+
+
+}
