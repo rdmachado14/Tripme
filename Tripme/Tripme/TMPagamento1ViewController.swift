5672d0093bb22fa67c79c2758fe212fc5582e20d
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-21 14:42:56 -0400

    Criando a classe de pagamento.

---
+++ Tripme/Tripme/TMPagamento1ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TMPagamento1ViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 1/21/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMPagamento1ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+
+}
