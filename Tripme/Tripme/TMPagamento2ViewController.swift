11c54ef010e5e6593cb74f3f611da3e2932fc036
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-22 11:56:28 -0400

    Adicionando a classe da view da segunda tela de pagamento.

---
+++ Tripme/Tripme/TMPagamento2ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TMPagamento2ViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 1/22/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMPagamento2ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+
+}
