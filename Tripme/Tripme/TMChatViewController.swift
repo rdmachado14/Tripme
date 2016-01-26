f3113428a36c811a960f4171ba5dc276994d672e
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-01-26 11:23:12 -0400

    Iniciando o chat.

---
+++ Tripme/Tripme/TMChatViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TMChatViewController.swift
+//  Tripme
+//
+//  Created by Rodrigo Machado on 1/26/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class TMChatViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
